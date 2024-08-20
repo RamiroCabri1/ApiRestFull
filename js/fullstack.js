cf752fd5b6e877b68cf074d65e86db72c7c38d0e
RamiroCabri <cabriramiro@gmail.com>
2024-08-20 16:58:38 -0300

    innecesario commit

--- js/fullstack.js
+++ js/fullstack.js
@@ -1,4 +1,4 @@
-fetch("https://66c4eb8eb026f3cc6cf110cb.mockapi.io/v1/person85")
+fetch("https://66c4eb8eb026f3cc6cf110cb.mockapi.io/v1/person")
 .then((valor)=> {console.log("hola rama", valor)})
 .catch((valor)=> {console.log("hola error", valor)})
 .finally(()=> {console.log("hola finalmente")})
