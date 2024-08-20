de3fb48d18a412c4f0e116a977eabe1b27e42b3b
RamiroCabri <cabriramiro@gmail.com>
2024-08-20 16:34:20 -0300

    "segundo dia"

--- js/fullstack.js
+++ js/fullstack.js
@@ -1,39 +1,4 @@
-const text = '{"nombre":"rama","dni":30388969, "fecha": "1893-08-07"}';
-
-/*const obj = JSON.parse(text);
-
-obj.fecha = Date(obj.fecha)*/
-
-
-const obj = JSON.parse( text , function(key, value){
-
-    if (key =="fecha"){
-
-        return  Date(value)
-
-    }else {
-
-        return value
-    }
-
-
-
-})
-
-
-console.table(obj)
-
-for ( let key in obj) {
-
-    console.log(key)
-}
-
-const objeto2 = {
-
-    name : "rama",
-    nickName : "dudemaster"
-
-}
-const texto2 = JSON.stringify(objeto2)
-
-console.log(texto2)
+fetch("https://66c4eb8eb026f3cc6cf110cb.mockapi.io/v1/person85")
+.then((valor)=> {console.log("hola rama", valor)})
+.catch((valor)=> {console.log("hola error", valor)})
+.finally(()=> {console.log("hola finalmente")})
